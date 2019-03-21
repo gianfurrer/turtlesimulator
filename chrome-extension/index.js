372ea4ce69dd61cd21f9d0b4e36508ad8293afcd
Silvan Büchi <silvan.buechi@roche.com>
2019-03-21 16:13:07 +0100

    Added simulator code

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,26 +1,45 @@
-const runLua = (code, callback) => {
-	body = "input="+code
+function runLua(code, callback) {
+	body = "input=" + code;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-    if (this.readyState == 4) {
-        if (this.status == 200) {
-            const httpResponse = document.createElement("html")
-            httpResponse.innerHTML = this.responseText
-            callback(httpResponse.querySelector("body > textarea").textContent)
+        if (this.readyState == XMLHttpRequest.DONE) {
+            if (this.status == 200) {
+                const httpResponse = document.createElement("html");
+                httpResponse.innerHTML = this.responseText;
+                callback(httpResponse.querySelector("body > textarea").textContent);
+            }
         }
-    }
     };
     xhttp.open("POST", "https://www.lua.org/cgi-bin/demo", true);
     xhttp.send(body);
 }
 
-
-// Removable Example Usages
-runLua("print('inline lamda call')", result => {
-	console.log(result)
-})
-
-function myFunc(result) {
-  console.log(result);
+function mergeCode(callback) {
+    getFileContent("simulator.lua", function (simulatorCode) {
+        userCode = document.getElementById("input").innerText;
+        callback(simulatorCode + userCode);
+    })
 }
-runLua("print('myFunc Call')", myFunc)
+
+function getFileContent(fileName, callback) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            callback(this.responseText);
+        }
+    };
+    xhttp.open("GET", chrome.runtime.getURL(fileName), true)
+    xhttp.send()
+}
+
+function execute() {
+    mergeCode(function (code) {
+        runLua(code, function (result) {
+            document.getElementById("output").innerText = result
+        })
+    })
+}
+
+onload = function () {
+    document.querySelector("#btn-execute").onclick = execute
+}
