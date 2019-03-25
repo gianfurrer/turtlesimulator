c23ac16cef25521f06129bddfaec43e4653bb3b0
Silvan Büchi <silvan.buechi@roche.com>
2019-03-25 08:23:43 +0100

    added new parser

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,18 +1,33 @@
-function runLua(code, callback) {
-	body = "input=" + code;
+const runLua = (code, callback) => {
+    const body = new FormData();
+    body.append("lang", "lua");
+    body.append("device", "");
+    body.append("code", code);
+    body.append("stdinput", "");
+    body.append("ext", "lua");
+    body.append("compile", "0");
+    body.append("execute", "lua main.lua");
+    body.append("mainfile", "main.lua");
+    body.append("uid", "ZumGlückIgnorieretsDeScheiss");
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE) {
-            if (this.status == 200) {
-                const httpResponse = document.createElement("html");
-                httpResponse.innerHTML = this.responseText;
-                callback(httpResponse.querySelector("body > textarea").textContent);
-            }
+    if (this.readyState == 4) {
+        if (this.status == 200) {
+            const httpResponse = document.createElement("html")
+            httpResponse.innerHTML = this.responseText
+            httpResponse.querySelector("span").remove();
+            callback(httpResponse.textContent);
         }
+    }
     };
-    xhttp.open("POST", "https://www.lua.org/cgi-bin/demo", true);
+    xhttp.open("POST", "https://tpcg.tutorialspoint.com/tpcg.php", true);
     xhttp.send(body);
 }
+
+// Removable Example Usages
+runLua("print('inline lamda call')", result => {
+  console.log(result)
+});
 
 function mergeCode(callback) {
     getFileContent("simulator.lua", (simulatorCode) => {
