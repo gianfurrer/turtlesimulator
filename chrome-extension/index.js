c9c856ef24188d864de3559ee3b9b09ca0bc8f92
Silvan Büchi <silvan.buechi@roche.com>
2019-03-27 16:26:16 +0100

    added semicolons

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -23,18 +23,17 @@ function getProgram(args) {
 }
 
 function getArgsCode(args) {
-    let argsCode = ""
+    let argsCode = "";
     if (args) {
         for (let i = 0; i < args.length; i++) {
-            argsCode += `arg[${i + 1}] = "${args[i]}"\n`
+            argsCode += `arg[${i + 1}] = "${args[i]}"\n`;
         }
     }
-
-    return argsCode
+    return argsCode;
 }
 
 function getArgs() {
-    const args = document.querySelector("#args").value
+    const args = document.querySelector("#args").value;
     return args.match(/(?<=(['"]).)(?:(?!\1|\\).|\\.)*(?=\1)|(\w+)/g);
 }
 
@@ -61,20 +60,20 @@ function getFileContent(fileName, callback) {
 }
 
 function executeProgram() {
-    const args = getArgs()
-    const program = getProgram(args)
+    const args = getArgs();
+    const program = getProgram(args);
     runLua(program, (response) => {
         document.querySelector("#output").value = response.Result;
         document.querySelector("#errors").value = response.Errors;
         document.querySelector("#warnings").value = response.Warnings;
-        simulate(response.Result)
+        simulate(response.Result);
     });
 }
 
 function generateInventory(slots) {
     const inventoryElement = document.querySelector("#inventory");
     for (let i = 0; i < slots; i++) {
-        const slot = {}
+        const slot = {};
         const inventorySlot = document.createElement("div");
         inventorySlot.id = `inventory-slot-${i + 1}`;
 
@@ -91,7 +90,7 @@ function generateInventory(slots) {
         inventorySlot.appendChild(slot.name);
         inventorySlot.appendChild(slot.count);
         inventoryElement.appendChild(inventorySlot);
-        inventory.push(slot)
+        inventory.push(slot);
     }
 }
 
@@ -103,89 +102,90 @@ const dict = {
 };
 
 function simulate(output) {
-    const lines = output.trim().split("\n")
+    const lines = output.trim().split("\n");
     for (let i = 0; i < lines.length; i++) {
         const components = lines[i].split(" ")
         const func = dict[components[0]];
-        func(...components.slice(1))
+        func(...components.slice(1));
     }
 }
 
-const directionEnum = { forward: 1, left: 2, back: 3, right: 4 }
-const inventory = []
-const blocks = []
-let selectedSlot = 1
-let fuelLevel = 0
-let x = 0
-let y = 0
-let z = 0
-let direction = directionEnum.forward
+const directionEnum = { forward: 1, left: 2, back: 3, right: 4 };
+const inventory = [];
+const blocks = [];
+let selectedSlot = 1;
+let fuelLevel = 0;
+let x = 0;
+let y = 0;
+let z = 0;
+let direction = directionEnum.forward;
 
 function setSelectedSlot(slot) {
-    selectedSlot = parseInt(slot)
+    selectedSlot = parseInt(slot);
+    document.querySelector("#selected-slot").textContent = selectedSlot;
 }
 
 function setFuelLevel(value) {
-    fuelLevel = parseInt(value)
-    document.querySelector("#fuel-level").style.height = fuelLevel / 100 + "px"
+    fuelLevel = parseInt(value);
+    document.querySelector("#fuel-level").textContent = fuelLevel;
 }
 
 function forward() {
-    if (direction == directionEnum.forward) { z += 1 }
-    else if (direction == directionEnum.back) { z -= 1 }
-    else if (direction == directionEnum.left) { x -= 1 }
-    else if (direction == directionEnum.right) { x += 1}
+    if (direction == directionEnum.forward) { z += 1; }
+    else if (direction == directionEnum.back) { z -= 1; }
+    else if (direction == directionEnum.left) { x -= 1; }
+    else if (direction == directionEnum.right) { x += 1; }
 }
 
 function back() {
-    if (direction == directionEnum.forward) { z -= 1 }
-    else if (direction == directionEnum.back) { z += 1 }
-    else if (direction == directionEnum.left) { x += 1 }
-    else if (direction == directionEnum.right) { x -= 1}
+    if (direction == directionEnum.forward) { z -= 1; }
+    else if (direction == directionEnum.back) { z += 1;}
+    else if (direction == directionEnum.left) { x += 1; }
+    else if (direction == directionEnum.right) { x -= 1; }
 }
 
 function up() {
-    y += 1
+    y += 1;
 }
 
 function down() {
-    y -= 1
+    y -= 1;
 }
 
 function turnLeft() {
-    if (direction == directionEnum.right) { direction = direction.forward }
-    else (direction += 1)
+    if (direction == directionEnum.right) { direction = direction.forward; }
+    else { direction += 1; }
 }
 
 function turnRight() {
     if (direction == directionEnum.forward) { direction = direction.right }
-    else (direction -= 1)
+    else { direction -= 1; }
 }
 
 function addBlock(name, x, y, z) {
-    blocks.push({ name: name, x: x, y: y, z: z })
+    blocks.push({ name: name, x: x, y: y, z: z });
 }
 
 function addItemToInventory(name, slot) {
     const inventorySlot = inventory[slot - 1];
     if (inventorySlot.name.value == "")
     {
-        inventorySlot.name.value = name
+        inventorySlot.name.value = name;
     }
-    inventorySlot.count.value = parseInt(inventorySlot.count.value) + 1
+    inventorySlot.count.value = parseInt(inventorySlot.count.value) + 1;
 }
 
 function removeItemFromInventory(quantity) {
-    inventory[selectedSlot - 1].count.value = parseInt(inventory[selectedSlot - 1].count.value) - parseInt(quantity)
+    inventory[selectedSlot - 1].count.value = parseInt(inventory[selectedSlot - 1].count.value) - parseInt(quantity);
     if (inventory[selectedSlot - 1].count.value == 0) {
-        inventory[selectedSlot - 1].name.value = ""
+        inventory[selectedSlot - 1].name.value = "";
     }
 }
 
 function initInventory(inv) {
   for (let i = 0; i < inv.length && inventory[i]; ++i) {
-    inventory[i].name.value = inv[i].name
-    inventory[i].count.value = inv[i].count
+    inventory[i].name.value = inv[i].name;
+    inventory[i].count.value = inv[i].count;
   }
 }
 
