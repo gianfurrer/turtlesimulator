c4ad46b60c38e98540b8a1effadefa26780f5771
Gian Furrer <gian.furrer@roche.com>
2019-03-28 10:15:44 +0100

    Compile lua with js

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,39 +1,32 @@
 
 let simulatorCode;
+this.simulator = {addAction: te => {console.log(te)}}
 
 getFileContent("simulator.lua", (content) => {
     simulatorCode = content;
 })
 
-function runLua (program, callback) {
-    const body = new FormData();
-    body.append("LanguageChoiceWrapper", "14");
-    body.append("Program", program);
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            callback(JSON.parse(this.responseText));
-    }};
-    xhttp.open("POST", "https://rextester.com/rundotnet/Run", true);
-    xhttp.send(body);
+function overrideLuaPrint(code, id) {
+  return 'local js = require "js"\nlocal output = js.global.document:querySelector("#output")\nlocal simulator = js.global.simulator\n\nfunction print(text)\noutput.textContent = string.format("%s%s\\n", output.textContent, text)\nsimulator:addAction(text)\nend\n\n'+code+'\nprint("[end] '+id+'")';
 }
 
-function getProgram(args) {
-    return getArgsCode(args) + "\n" + simulatorCode + "\n" + getInventoryCode(startInventory) + "\n" + document.querySelector("#input").value;
+function getProgram(args, id) {
+    return overrideLuaPrint(getArgsCode(args) + "\n" + simulatorCode + "\n" + getInventoryCode(startInventory) + "\n" + document.querySelector("#input").value, id);
 }
 
 function getArgsCode(args) {
-    let argsCode = "";
+    let argsCode = ""
     if (args) {
         for (let i = 0; i < args.length; i++) {
-            argsCode += `arg[${i + 1}] = "${args[i]}"\n`;
+            argsCode += `arg[${i + 1}] = "${args[i]}"\n`
         }
     }
-    return argsCode;
+
+    return argsCode
 }
 
 function getArgs() {
-    const args = document.querySelector("#args").value;
+    const args = document.querySelector("#args").value
     return args.match(/(?<=(['"]).)(?:(?!\1|\\).|\\.)*(?=\1)|(\w+)/g);
 }
 
@@ -41,7 +34,7 @@ function getInventoryCode(inventory) {
     let inventoryCode = "inventory = {\n";
     for (let i = 0; i < inventory.length; i++) {
         const slot = inventory[i];
-        inventoryCode += `{ name = "${slot.name.value}", count = ${slot.name.value} },\n`;
+        inventoryCode += `{ name = "${slot.name.value}", count = ${slot.count.value} },\n`;
     }
     inventoryCode += "}\n";
     return inventoryCode;
@@ -60,13 +53,17 @@ function getFileContent(fileName, callback) {
 
 function executeProgram() {
     const args = getArgs();
-    const program = getProgram(args);
-    runLua(program, (response) => {
-        document.querySelector("#output").value = response.Result;
-        document.querySelector("#errors").value = response.Errors;
-        document.querySelector("#warnings").value = response.Warnings;
-        simulate(response.Result);
-    });
+    const id = new Date().getTime();
+    const program = getProgram(args, id);
+
+    window.simulator = new Simulator();
+
+    const script = document.createElement("script")
+    script.id = id;
+    script.type = "application/lua";
+    script.textContent = program;
+
+    document.body.appendChild(script);
 }
 
 function generateInventory(inventoryElement, slots) {
@@ -102,31 +99,33 @@ function initInventory(inventory, values) {
   }
 }
 
-function simulate(output) {
-    if (output) {
-      simulator = new Simulator()
-      const lines = output.trim().split("\n")
-      for (let i = 0; i < lines.length; i++) {
-          const components = lines[i].split(" ")
-          const func = simulator.dict[components[0]];
-          func(...components.slice(1))
-      }
-    }
-}
-
 const directionEnum = { forward: 1, left: 2, back: 3, right: 4 };
 const output = document.querySelector("#output");
 function Simulator() {
-  this.inventory = generateInventory(16)
-  initInventory(document.querySelector("#liveInventory"), startInventory);
+  const invElement = document.querySelector("#liveInventory")
+  this.inventory = generateInventory(invElement, 16);
+  initInventory(this.inventory, startInventory.map(s => [{name: s.name.value, count: s.count.value}][0]));
   this.blocks = [];
   this.selectedSlot = 1;
-  this.fuelLevel = 0
+  this.fuelLevel = 0;
   this.x = this.y = this.z = 0;
   this.direction = directionEnum.forward;
   this.programCounter = 0;
   output.innerHTML = "";
+  this.actions = [];
 
+  this.addAction = action => {
+    this.actions.push(action);
+  }
+
+  this.intervalId = setInterval(() => {
+    if (this.actions.length) {
+      const action = this.actions.shift();
+      const components = action.split(" ");
+      const func = this.dict[components[0]];
+      func(...components.slice(1));
+    }
+  }, 100);
 
   this.setSelectedSlot = (slot) => {
       this.selectedSlot = parseInt(slot)
@@ -173,23 +172,29 @@ function Simulator() {
   }
 
   this.addItemToInventory = (name, slot) => {
-      const inventorySlot = inventory[slot - 1];
-      if (inventorySlot.name == "")
+      const inventorySlot = this.inventory[slot - 1];
+      if (inventorySlot.name.value == "")
       {
-          inventorySlot.name = name
+          inventorySlot.name.value = name;
       }
-      inventorySlot.count = parseInt()
+      inventorySlot.count.value = 0;
   }
 
   this.removeItemFromInventory = (quantity) => {
-      inventory[this.selectedSlot - 1].count = inventory[this.selectedSlot - 1].count - quantity
+      this.inventory[this.selectedSlot - 1].count.value -= quantity
+  }
+
+  this.end = id => {
+    clearInterval(this.intervalId);
+    Array.from(document.querySelectorAll("script")).filter(s => s.id == id)[0].remove();
   }
 
   this.dict = {
       "[setSelectedSlot]": this.setSelectedSlot, "[setFuelLevel]": this.setFuelLevel,
       "[forward]": this.forward, "[up]": this.up, "[down]": this.down, "[back]": this.back,
       "[turnLeft]": this.turnLeft, "[turnRight]": this.turnRight,
-      "[addBlock]": this.addBlock, "[addItemToInventory]": this.addItemToInventory, "[removeItemFromInventory]": this.removeItemFromInventory
+      "[addBlock]": this.addBlock, "[addItemToInventory]": this.addItemToInventory, "[removeItemFromInventory]": this.removeItemFromInventory,
+      "[end]": this.end
   };
 }
 
@@ -199,4 +204,10 @@ onload = () => {
     const startInventoryElement = document.querySelector("#startInventory");
     startInventory = generateInventory(startInventoryElement, 16);
     initInventory(startInventory, [{name: "minecraft:coal_block", count: 64}]);
+}
+
+onerror = e => {
+  if (e.startsWith('[string "..."]')) {
+    document.querySelector("#errors").textContent = e.split(":").splice(2).join(":").trim();
+  }
 }
