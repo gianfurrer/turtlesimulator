d492a4f56062d3e07ced2ce9bfe91041f8b5803c
Silvan Büchi <silvan.buechi@roche.com>
2019-04-02 15:44:40 +0200

    lolz

--- chrome-extension/inventory.js
+++ chrome-extension/inventory.js
@@ -1,34 +1,65 @@
-const itemsElement = document.querySelector("#items");
-items.forEach(item => {
-    const wrapper = document.createElement("div");
-    wrapper.id = item.value;
-    wrapper.value = item.value;
-    wrapper.label = item.label;
-    wrapper.backgroundCss = item.backgroundCss;
-    wrapper.classList.add("item");
+function ItemModal(slot) {
 
-    const image = document.createElement("div");
-    image.classList.add("item-image");
-    image.style.background = item.backgroundCss;
+    this.init = () => {
+        var modalElement = document.createElement("div");
 
-    const tooltip = document.createElement("span");
-    tooltip.classList.add("item-tooltip");
-    tooltip.innerText = item.label + " (" + item.value + ")";
+        const itemsElement = document.createElement("div");
+        items.forEach(item => {
+            const wrapper = document.createElement("div");
+            wrapper.id = item.value;
+            wrapper.value = item.value;
+            wrapper.label = item.label;
+            wrapper.backgroundCss = item.backgroundCss;
+            wrapper.classList.add("item");
+        
+            const image = document.createElement("div");
+            image.classList.add("item-image");
+            image.style.background = item.backgroundCss;
+        
+            const tooltip = document.createElement("span");
+            tooltip.classList.add("item-tooltip");
+            tooltip.innerText = item.label + " (" + item.value + ")";
+        
+            wrapper.appendChild(image);
+            wrapper.appendChild(tooltip);
+            wrapper.onclick = this.onItemClicked;
+            itemsElement.appendChild(wrapper);
+        });
+        modalElement.appendChild(itemsElement);
 
-    wrapper.appendChild(image);
-    wrapper.appendChild(tooltip);
-    itemsElement.appendChild(wrapper);
-});
+        const queryElement = document.createElement("input");
+        queryElement.type = "text";
+        queryElement.onkeyup = queryElement.onchange = e => {
+            const query = e.target.value.toLowerCase();
+            Array.from(itemsElement.children).forEach(item => {
+                if (item.label.toLowerCase().includes(query) || item.value.toLowerCase().includes(query)) {
+                    item.style.display = "inline-block";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+        };
+        modalElement.appendChild(queryElement);
+        document.querySelector("main").appendChild(modalElement);
+    }
 
-const itemSearch = document.querySelector("#item-search");
-itemSearch.onkeyup = itemSearch.onchange = e => {
-    const query = e.target.value.toLowerCase();
-    Array.from(itemsElement.children).forEach(item => {
-        if (item.label.toLowerCase().includes(query) || item.value.toLowerCase().includes(query)) {
-            item.style.display = "inline-block";
-        } else {
-            item.style.display = "none";
-        }
-    });
-};
+    this.onItemClicked = e => {
+        slot.name.value = e.currentTarget.value;
+        this.close()
+    }
 
+    this.open = () => {
+        document.querySelector("main").appendChild(modalElement);
+    }
+
+    this.close = () => {
+        modalElement.remove();
+    }
+
+    this.slot = slot;
+    this.init();
+}
+
+const openItemModal = slot => {
+    const modal = new ItemModal(slot);
+}
