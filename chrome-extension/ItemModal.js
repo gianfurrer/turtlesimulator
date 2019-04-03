dc499bde22d1323ea40d140f68a7b3b5d55cf119
Silvan Büchi <silvan.buechi@roche.com>
2019-04-03 10:39:57 +0200

    itemmodal improvement

--- chrome-extension/ItemModal.js
+++ chrome-extension/ItemModal.js
@@ -1,10 +1,6 @@
-function ItemModal(slot) {
-    this.slot = slot;
-    this.modal;
-
-    this.init = () => {
-        this.modal = document.createElement("div");
-
+function ItemModal() {
+    this.generateDOM = () => {
+        const modalElement = document.createElement("div");
         const itemsElement = document.createElement("div");
         items.forEach(item => {
             const wrapper = document.createElement("div");
@@ -13,15 +9,15 @@ function ItemModal(slot) {
             wrapper.label = item.label;
             wrapper.backgroundCss = item.backgroundCss;
             wrapper.classList.add("item");
-        
+
             const image = document.createElement("div");
             image.classList.add("item-image");
             image.style.background = item.backgroundCss;
-        
+
             const tooltip = document.createElement("span");
             tooltip.classList.add("item-tooltip");
             tooltip.innerText = item.label + " (" + item.value + ")";
-        
+
             wrapper.appendChild(image);
             wrapper.appendChild(tooltip);
             wrapper.onclick = this.onItemClicked;
@@ -33,7 +29,10 @@ function ItemModal(slot) {
         queryElement.onkeyup = queryElement.onchange = e => {
             const query = e.target.value.toLowerCase();
             Array.from(itemsElement.children).forEach(item => {
-                if (item.label.toLowerCase().includes(query) || item.value.toLowerCase().includes(query)) {
+                if (
+                    item.label.toLowerCase().includes(query) ||
+                    item.value.toLowerCase().includes(query)
+                ) {
                     item.style.display = "inline-block";
                 } else {
                     item.style.display = "none";
@@ -41,28 +40,40 @@ function ItemModal(slot) {
             });
         };
 
-        this.modal.appendChild(queryElement);
-        this.modal.appendChild(itemsElement);
-    }
+        const countElement = document.createElement("input");
+        countElement.type = "number";
+        countElement.min = "1";
+        countElement.max = "64";
 
-    this.onItemClicked = e => {
-        this.slot.nameElement.value = e.currentTarget.value;
-        this.slot.imageElement.style.background = e.currentTarget.backgroundCss;
-        this.close()
-    }
+        modalElement.appendChild(queryElement);
+        modalElement.appendChild(countElement);
+        modalElement.appendChild(itemsElement);
 
-    this.open = () => {
-        document.querySelector("main").appendChild(this.modal);
-    }
+        return modalElement;
+    };
+
+    this.open = slot => {
+        if (slot) {
+            this.slot = slot;
+        }
+        document.body.appendChild(this.modal);
+    };
 
     this.close = () => {
-        this.modal.remove()
-    }
+        this.modal.remove();
+    };
 
-    this.init();
+    this.onItemClicked = e => {
+        this.slot.countElement.value = this.modal.children[1].value;
+        this.slot.nameElement.value = e.currentTarget.value;
+        this.slot.imageElement.style.background = e.currentTarget.backgroundCss;
+        this.close();
+    };
+
+    this.modal = this.generateDOM();
 }
 
+const itemModal = new ItemModal();
 const openItemModal = e => {
-    const modal = new ItemModal(e.currentTarget);
-    modal.open();
-}
+    itemModal.open(e.currentTarget);
+};
