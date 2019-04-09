0ec4fb50eebec63cfe71df2a3a0fcd990f1efe22
Silvan Büchi <silvan.buechi@roche.com>
2019-04-09 11:43:23 +0200

    lolz

--- chrome-extension/js/index.js
+++ chrome-extension/js/index.js
@@ -115,192 +115,53 @@ function initInventory(inventory, values) {
     for (let i = 0; i < values.length && inventory[i]; ++i) {
         const name = values[i].name;
         inventory[i].nameElement.value = name;
-        if (values[i].name == "") {
-            inventory[i].imageElement.style.background = "";       
-        } else {
-            inventory[i].imageElement.style.background = items.filter(i => i.value === name)[0].backgroundCss
-        }
         inventory[i].countElement.value = values[i].count;
+        const item = items.filter(i => i.value === name)[0];
+        inventory[i].imageElement.style.background = item ? item.backgroundCss : "";
     }
 }
 
-function Simulator(program) {
-    this.map = generateMap(document.querySelector("#chunks").value);
+let chunksElement;
+let outputElement;
+let errorOutputElement;
+let simulator3dElement;
+let startInventoryElement;
+let liveInventoryElement;
+let selectedSlotElement;
+let fuelLevelElement;
+let positionXElement;
+let positionYElement;
+let positionZElement;
+let directionElement;
 
-    const directionEnum = { forward: 1, left: 2, back: 3, right: 4 };
-    const output = document.querySelector("#output");
-    const errorOutput = document.querySelector("#erorrs");
-
-    this.simulator3d = new Simulator3D(document.querySelector("#simulator3d"));
-    this.simulator3d.initCanvas(this.map);
-    this.inventory = liveInventory;
-    initInventory(
-        this.inventory,
-        startInventory.map(
-            slot => [{ name: slot.nameElement.value, count: slot.countElement.value }][0]
-        )
-    );
-    this.blocks = [];
-    this.selectedSlot = 1;
-    this.fuelLevel = 0;
-    this.x = this.y = this.z = 0;
-    this.direction = directionEnum.forward;
-    this.states = [];
-
-    output.innerHTML = "";
-
-    this.luaWorker = new Worker("js/lua-worker.js");
-    this.luaWorker.onmessage = e => {
-        const actionElement = document.createElement("div");
-        actionElement.innerText = e.data;
-        output.appendChild(actionElement);
-        this.executeAction(actionElement);
-    };
-    this.luaWorker.postMessage(program);
-
-    this.executeAction = actionElement => {
-        const components = actionElement.innerText.split(" ");
-        const func = this.dict[components[1]];
-        func(...components.slice(2));
-        this.saveState(actionElement);
-    };
-
-    const stateElement = document.querySelector("#state");
-    stateElement.oninput = e => this.applyState(e.target.value);
-
-    this.saveState = actionElement => {
-        this.states.push({
-            actionElement: actionElement,
-            inventory: this.inventory.map(
-                i => [{ name: i.nameElement.value, count: i.countElement.value, backgroundCss: i.imageElement.style.background }][0]
-            ),
-            blocks: JSON.parse(JSON.stringify(this.blocks)),
-            selectedSlot: this.selectedSlot,
-            fuelLevel: this.fuelLevel,
-            x: this.x,
-            y: this.y,
-            z: this.z,
-            direction: this.direction
-        });
-        stateElement.max = this.states.length - 1;
-        stateElement.value = this.states.length - 1;
-    };
-
-    this.applyState = index => {
-        const state = this.states[index];
-        for (let i = 0; i < this.inventory.length; i++) {
-            this.inventory[i].nameElement.value = state.inventory[i].name;
-            this.inventory[i].imageElement.style.background = state.inventory[i].backgroundCss;
-            this.inventory[i].countElement.value = state.inventory[i].count;
-        }
-        this.blocks = state.blocks;
-        this.setSelectedSlot(state.selectedSlot);
-        this.setFuelLevel(state.fuelLevel);
-        this.move(state.x, state.y, state.z);
-        this.turn(state.direction);
-        stateElement.value = index;
-    };
-
-    this.play = () => {
-        const timeout = document.querySelector("#timeout").value;
-        let i = stateElement.value;
-        const intervalId = setInterval(() => {
-            if (i < this.states.length) {
-                this.applyState(i++);
-            } else {
-                clearInterval(intervalId);
-            }
-        }, timeout);
-    };
-    document.querySelector("#play").onclick = this.play;
-
-    const selectedSlotElement = document.querySelector("#selected-slot");
-    this.setSelectedSlot = slot => {
-        this.selectedSlot = parseInt(slot);
-        selectedSlotElement.textContent = this.selectedSlot;
-    };
-
-    const fuelLevelElement = document.querySelector("#fuel-level");
-    this.setFuelLevel = value => {
-        this.fuelLevel = parseInt(value);
-        fuelLevelElement.textContent = this.fuelLevel;
-    };
-
-    const positionXElement = document.querySelector("#position-x");
-    const positionYElement = document.querySelector("#position-y");
-    const positionZElement = document.querySelector("#position-z");
-    this.move = (x, y, z) => {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        positionXElement.textContent = this.x;
-        positionYElement.textContent = this.y;
-        positionZElement.textContent = this.z;
-    };
-
-    const directionElement = document.querySelector("#direction");
-    this.turn = direction => {
-        this.direction = direction;
-        directionElement.textContent = this.direction
-    };
-
-    this.addBlock = (name, x, y, z) => {
-        const block = { name: name, x: x, y: y, z: z };
-        this.simulator3d.addBlock(block);
-        this.blocks.push(block);
-    };
-
-    this.addItemToInventory = (name, slot) => {
-        const inventorySlot = this.inventory[slot - 1];
-        if (inventorySlot.nameElement.value == "") {
-            inventorySlot.nameElement.value = name;
-            inventorySlot.imageElement.style.background = items.filter(i => i.value === name)[0].backgroundCss;
-        }
-        inventorySlot.countElement.value = parseInt(inventorySlot.countElement.value) + 1;
-    };
-
-    this.removeItemFromInventory = quantity => {
-        this.inventory[this.selectedSlot - 1].countElement.value -= quantity;
-        if (this.inventory[this.selectedSlot - 1].countElement.value == 0) {
-            this.inventory[this.selectedSlot - 1].nameElement.value = "";
-            this.inventory[this.selectedSlot - 1].imageElement.style.background = "";
-        }
-    };
-
-    this.error = text => {
-        errorOutput.value += text;
-    };
-
-    this.end = () => {
-        clearInterval(this.intervalId);
-        this.luaWorker.terminate();
-    };
-
-    this.dict = {
-        "[select]": this.setSelectedSlot,
-        "[setFuelLevel]": this.setFuelLevel,
-        "[move]": this.move,
-        "[turn]": this.turn,
-        "[addBlock]": this.addBlock,
-        "[addItemToInventory]": this.addItemToInventory,
-        "[removeItemFromInventory]": this.removeItemFromInventory,
-        "[drop]": this.removeItemFromInventory,
-        "[error]": this.error,
-        "[start]": () => {},
-        "[end]": this.end
-    };
-}
+let stateElement;
+let playElement;
 
 let startInventory;
+let liveInventory;
+
 onload = () => {
+    chunksElement = document.querySelector("#chunks");
+    outputElement = document.querySelector("#output");
+    errorOutputElement = document.querySelector("#error-output");
+    simulator3dElement = document.querySelector("#simulator3d");
+    startInventoryElement = document.querySelector("#start-inventory");
+    liveInventoryElement = document.querySelector("#live-inventory");
+    selectedSlotElement = document.querySelector("#selected-slot");
+    fuelLevelElement = document.querySelector("#fuel-level");
+    positionXElement = document.querySelector("#position-x");
+    positionYElement = document.querySelector("#position-y");
+    positionZElement = document.querySelector("#position-z");
+    directionElement = document.querySelector("#direction");
+    playElement = document.querySelector("#play");
+    stateElement = document.querySelector("#state");
     document.querySelector("#btn-execute").onclick = executeProgram;
-    const startInventoryElement = document.querySelector("#startInventory");
-    const liveInventoryElement = document.querySelector("#liveInventory");
     startInventory = generateInventory(startInventoryElement, 16);
     liveInventory = generateInventory(liveInventoryElement, 16);
     initInventory(startInventory, [
         { name: "minecraft:coal_block", count: 64 }
     ]);
+    
 };
 
 onerror = e => {
---
+++ chrome-extension/js/simulator.js
@@ -0,0 +1,204 @@
+const directionEnum = { forward: 1, left: 2, back: 3, right: 4 };
+
+class Simulator {
+    constructor(program) {
+        this.map = generateMap(chunksElement.value);
+        this.simulator3d = new Simulator3D(simulator3dElement);
+        this.simulator3d.initCanvas(this.map);
+        this.inventory = liveInventory;
+        outputElement.innerHTML = "";
+        initInventory(
+            this.inventory,
+            startInventory.map(
+                slot =>
+                    [
+                        {
+                            name: slot.nameElement.value,
+                            count: slot.countElement.value
+                        }
+                    ][0]
+            )
+        );
+        
+        this.selectedSlot = 1;
+        this.fuelLevel = 0;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.positionZ = 0;
+        this.direction = directionEnum.forward;
+        this.blocks = [];
+        this.states = [];
+        
+        stateElement.oninput = e => this.applyState(e.target.value);
+        playElement.onclick = this.play
+
+        this.luaWorker = new Worker("js/lua-worker.js");
+        this.luaWorker.onmessage = e => {
+            const actionElement = document.createElement("div");
+            actionElement.innerText = e.data;
+            output.appendChild(actionElement);
+            this.executeAction(actionElement);
+        };
+        this.luaWorker.postMessage(program);
+    }
+
+    executeAction = actionElement => {
+        const components = actionElement.innerText.split(" ");
+        const func = this.funcNameToFuncDict[components[1]];
+        func(...components.slice(2));
+        this.saveState(actionElement);
+    };
+
+    saveState = actionElement => {
+        this.states.push({
+            actionElement: actionElement,
+            inventory: this.inventory.map(
+                i => [{ name: i.nameElement.value, count: i.countElement.value, backgroundCss: i.imageElement.style.background }][0]
+            ),
+            blocks: JSON.parse(JSON.stringify(this.blocks)),
+            selectedSlot: this.selectedSlot,
+            fuelLevel: this.fuelLevel,
+            x: this.positionX,
+            y: this.positionY,
+            z: this.positionZ,
+            direction: this.direction
+        });
+        stateElement.max = this.states.length - 1;
+        stateElement.value = this.states.length - 1;
+    };
+
+    applyState = index => {
+        const state = this.states[index];
+        for (let i = 0; i < this.inventory.length; i++) {
+            this.inventory[i].nameElement.value = state.inventory[i].name;
+            this.inventory[i].imageElement.style.background = state.inventory[i].backgroundCss;
+            this.inventory[i].countElement.value = state.inventory[i].count;
+        }
+        this.blocks = state.blocks;
+        this.setSelectedSlot(state.selectedSlot);
+        this.setFuelLevel(state.fuelLevel);
+        this.move(state.x, state.y, state.z);
+        this.turn(state.direction);
+        stateElement.value = index;
+    };
+
+    play = () => {
+        const timeout = document.querySelector("#timeout").value;
+        let i = stateElement.value;
+        const intervalId = setInterval(() => {
+            if (i < this.states.length) {
+                this.applyState(i++);
+            } else {
+                clearInterval(intervalId);
+            }
+        }, timeout);
+    };
+
+    setSelectedSlot = slot => {
+        this.selectedSlot = slot;
+    };
+
+    setFuelLevel = value => {
+        this.fuelLevel = value;
+    };
+
+    move = (x, y, z) => {
+        this.positionX = x;
+        this.positionY = y;
+        this.positionZ = z;
+    };
+
+    turn = direction => {
+        this.direction = direction;
+    };
+
+    addBlock = (name, x, y, z) => {
+        const block = { name: name, x: x, y: y, z: z };
+        this.simulator3d.addBlock(block);
+        this.blocks.push(block);
+    };
+
+    addItemToInventory = (name, slot) => {
+        const inventorySlot = this.inventory[slot - 1];
+        if (inventorySlot.nameElement.value == "") {
+            inventorySlot.nameElement.value = name;
+            inventorySlot.imageElement.style.background = items.filter(i => i.value === name)[0].backgroundCss;
+        }
+        inventorySlot.countElement.value = parseInt(inventorySlot.countElement.value) + 1;
+    };
+
+    removeItemFromInventory = quantity => {
+        const slot = this.selectedSlot;
+        this.inventory[this.selectedSlot - 1].countElement.value -= quantity;
+        if (this.inventory[this.selectedSlot - 1].countElement.value == 0) {
+            this.inventory[this.selectedSlot - 1].nameElement.value = "";
+            this.inventory[this.selectedSlot - 1].imageElement.style.background = "";
+        }
+    };
+
+    error = text => {
+        errorOutputElement.value += text;
+    };
+
+    end = () => {
+        clearInterval(this.intervalId);
+        this.luaWorker.terminate();
+    };
+
+    funcNameToFuncDict = {
+        "[select]": this.setSelectedSlot,
+        "[setFuelLevel]": this.setFuelLevel,
+        "[move]": this.move,
+        "[turn]": this.turn,
+        "[addBlock]": this.addBlock,
+        "[addItemToInventory]": this.addItemToInventory,
+        "[removeItemFromInventory]": this.removeItemFromInventory,
+        "[drop]": this.removeItemFromInventory,
+        "[error]": this.error,
+        "[start]": () => {},
+        "[end]": this.end
+    };
+
+
+    set selectedSlot(value) {
+        selectedSlotElement.textContent = value;
+    }
+    get selectedSlot() {
+        return parseInt(selectedSlotElement.textContent);
+    }
+
+    set fuelLevel(value) {
+        fuelLevelElement.textContent = value;
+    }
+    get fuelLevel() {
+        return parseInt(fuelLevelElement.textContent);
+    }
+
+    set positionX(value) {
+        positionXElement.textContent = value;
+    }
+    get positionX() {
+        return parseInt(positionXElement.textContent);
+    }
+
+    set positionY(value) {
+        positionYElement.textContent = value;
+    }
+    get positionY() {
+        return parseInt(positionYElement.textContent);
+    }
+
+    set positionZ(value) {
+        positionZElement.textContent = value;
+    }
+    get positionZ() {
+        return parseInt(positionZElement.textContent);
+    }
+
+    set direction(value) {
+        directionElement.textContent = value;
+    }
+    get direction() {
+        return parseInt(directionElement.textContent);
+    }
+}
